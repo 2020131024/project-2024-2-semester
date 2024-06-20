03daa5a3d7ec626b3a5dcc28faf4c9d9169979a9
2020131024 <singcrazy@naver.com>
2024-06-21 01:40:41 +0900

    Update Project2.py

--- Project2.py
+++ Project2.py
@@ -10,7 +10,7 @@ a = df['RANK']
 b = df['타율']
 c = df['OPS']
 d = df['0.1WAR']
-e = df['0.3WRC+']
+e = df['0.003WRC+']
 
 a_val = a.values
 b_val = b.values
@@ -27,13 +27,13 @@ e_list = e_val.tolist()
 
 plt.rcParams['font.family'] = 'NanumGothicOTF'
 
-dff = pd.DataFrame({'RANK':a_list, '타율':b_list, 'OPS':c_list, '0.3WRC+':e_list})
-ax = dff.plot(x='RANK', y=['타율', 'OPS', '0.3WRC+'], marker='o')
+dff = pd.DataFrame({'RANK':a_list, '타율':b_list, 'OPS':c_list, '0.003WRC+':e_list})
+ax = dff.plot(x='RANK', y=['타율', 'OPS', '0.003WRC+'], marker='o')
 plt.xticks(np.arange(0,20,1))
 ax.set_xlabel('RANK')
 ax.set_ylabel('Unit')
 
-ax.legend(['타율', 'OPS', '0.3WRC+'], loc='upper left')
+ax.legend(['타율', 'OPS', '0.003WRC+'], loc='upper left')
 
 ax.set_title('타율과 OPS 및 WRC+의 상관관계')
 
