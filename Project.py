677a97ad00d70575f6159719006ba928b013e8c7
2020131024 <singcrazy@naver.com>
2024-06-21 00:53:38 +0900

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 df=pd.read_csv('C:/Users/조진우/PycharmProjects/pythonProject/data3.csv', encoding='cp949')
-df.info()
 
-a = df['이름']
+
+a = df['RANK']
 b = df['타율']
 c = df['OPS']
-d = df['WAR']
-e = df['WRC+']
+d = df['0.1WAR']
+e = df['0.3WRC+']
 
 a_val = a.values
 b_val = b.values
@@ -22,14 +24,17 @@ c_list = c_val.tolist()
 d_list = d_val.tolist()
 e_list = e_val.tolist()
 
-dff = pd.DataFrame({'이름': a_list, '타율': b_list, 'OPS': c_list, 'WAR': d_list})
-ax = dff.plot(x='이름', y=['타율', 'OPS', 'WAR'], marker='o')
-ax.set_xlabel('이름')
+
+plt.rcParams['font.family'] = 'NanumGothicOTF'
+
+dff = pd.DataFrame({'RANK':a_list, '타율':b_list, 'OPS':c_list, '0.1WAR':d_list})
+ax = dff.plot(x='RANK', y=['타율', 'OPS', '0.1WAR'], marker='o')
+plt.xticks(np.arange(0,20,1))
+ax.set_xlabel('RANK')
 ax.set_ylabel('Unit')
-ax.legend(['타율', 'OPS', 'WAR'], loc='upper left')
-ax.set_xticks(a_list)
-ax.set_yticks([0, 10])
+
+ax.legend(['타율', 'OPS', '0.1WAR'], loc='upper left')
+
 ax.set_title('타율과 OPS 및 WAR의 상관관계')
 
 plt.show()
-
