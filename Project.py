6cc6043c07fad674c29dffe22f2085a7c67a8bc5
2020131024 <singcrazy@naver.com>
2024-06-20 23:42:46 +0900

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,50 +1,35 @@
-from itertools import combinations
+import pandas as pd
+import matplotlib.pyplot as plt
 
-#임의의 리스트의 부분집합을 구하는 itertools 사용
-def sub_lists(my_list):
-    subs = []
+df=pd.read_csv('C:/Users/조진우/PycharmProjects/pythonProject/data3.csv', encoding='cp949')
+df.info()
 
-    for i in range(0, len(my_list) + 1):
+a = df['이름']
+b = df['타율']
+c = df['OPS']
+d = df['WAR']
+e = df['WRC+']
 
-        temp = [list(x) for x in combinations(my_list, i)]
+a_val = a.values
+b_val = b.values
+c_val = c.values
+d_val = d.values
+e_val = e.values
 
-        if len(temp) > 0:
-            subs.extend(temp)
+a_list = a_val.tolist()
+b_list = b_val.tolist()
+c_list = c_val.tolist()
+d_list = d_val.tolist()
+e_list = e_val.tolist()
 
-    return subs
+dff = pd.DataFrame({'이름': a_list, '타율': b_list, 'OPS': c_list, 'WAR': d_list})
+ax = dff.plot(x='이름', y=['타율', 'OPS', 'WAR'], marker='o')
+ax.set_xlabel('이름')
+ax.set_ylabel('Unit')
+ax.legend(['타율', 'OPS', 'WAR'], loc='upper left')
+ax.set_xticks(a_list)
+ax.set_yticks([0, 10])
+ax.set_title('타율과 OPS 및 WAR의 상관관계')
 
-#input data로 정수 리스트 생성하기 ex)1 3 2 > [1, 3, 2]
-a = list(map(int, input('입력:').split()))
+plt.show()
 
-#받은 리스트를 오름차순으로 정렬 ex)[1, 3, 2] > [1, 2, 3]
-arr = sorted(a)
-
-#n개의 정수를 받으므로 리스트 길이를 활용해 n 설정
-n = len(arr)
-
-#오름차순으로 되어있는 리스트의 부분집합 구하기 ex)[1, 2, 3] > [[], [1], [2], [3], [1.2],..., [1,2,3]]
-b = sub_lists(arr)
-
-#for문을 활용하여 부분집합 중 [] 원소 제거
-All = []
-for i in range(len(b)):
-    if len(b[i]) >= 1:
-        All.append(list(b[i]))
-
-#필요시 중간점검
-#print(All)
-
-#코드 목적에 부합하는 부분집합을 구하기 위해 for문을 활용해 각 list의 원소의 합이 n의 배수인치 확인하고 분류
-multiple_n = []
-for i in range((2**n)-1):
-    if sum(All[i]) % n == 0:
-        multiple_n.append(list(All[i]))
-
-#필요시 중간점검
-#print(multiple_n)
-
-#원하는 조건을 만족하는 리스트들을 각 리스트의 길이별로 분류해서 출력
-for i in range(len(multiple_n)):
-    for j in range(1,n+1):
-        if len(multiple_n[i]) == j:
-            print(f'길이가 {j}인 리스트는 {multiple_n[i]}')
